79c20f01f19f80801b1463cfee4e62f45e77d728
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2022-01-27 11:34:01 +0600

    Update PrintJob.h

--- DesignPattern/PrintJob.h
+++ DesignPattern/PrintJob.h
@@ -10,12 +10,17 @@ class PrintJob
 {
 	private:
 	queue<PrintRequest> PrintRequests;
-
+	/* PrioritySetting class is needed to order the PrintRequest so, the class sent to PrintRequest
+	PrioritySetting PrioritySetter;
+	*/
 		void sendToProduction(PrintRequest);
 	public:
 		PrintJob();
 		void addJob(PrintRequest);
 		void pullJob(PrintRequest);
+		/* Single-Responsibility Principle violated. ChangePriority function passed to PrioritySetting Class 
+		void changePriority();
+		*/
 };
 
 #endif // PRINTJOB_H
